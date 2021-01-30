2bf1d6a12c3ebeebfff79320450f04a7059059ea
gudao <2873908996@qq.com>
2021-01-30 17:15:06 +0800

    点对点消息Demo

---
+++ src/main/java/com/gudao/activemq/demo/Consumer.java
@@ -0,0 +1,49 @@
+package com.gudao.activemq.demo;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.Message;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+/**
+ * 消费者
+ * Author : GuDao
+ * 2021-01-30
+ */
+
+public class Consumer {
+    private static final String ACTIVEMQ_URL = "tcp://000.000.000:61616";
+
+    public static void main(String[] args) throws JMSException {
+        // 创建连接工厂
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
+        // 创建连接
+        Connection connection = activeMQConnectionFactory.createConnection();
+        // 打开连接
+        connection.start();
+        // 创建会话
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        // 创建队列目标,并标识队列名称，消费者根据队列名称接收数据
+        Destination destination = session.createQueue("myQueue");
+        // 创建消费者
+        MessageConsumer consumer = session.createConsumer(destination);
+        // 创建消费的监听
+        consumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(javax.jms.Message message) {
+                TextMessage textMessage = (TextMessage) message;
+                try {
+                    System.out.println("消费的消息：" + textMessage.getText());
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
